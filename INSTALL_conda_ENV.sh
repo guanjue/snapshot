e9c3a6fc80cc4a2664cf11a30cf4f6710e325bba
gxiang <gxiang@camp4tx.com>
2023-02-03 12:39:48 -0500

    clean home folder

--- INSTALL_conda_ENV.sh
+++
@@ -1,7 +0,0 @@
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
-conda config --add channels mvdbeek
-
-conda create -n snapshot python=3 r=3.6 bedtools ucsc_tools numpy scikit-learn r-ggplot2 r-pheatmap r-igraph r-networkD3 r-data.table r-mclust r-dplyr r-lsa r-cba r-RColorBrewer r-tidyverse
-conda activate snapshot
